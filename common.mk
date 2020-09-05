16ecd3506d17fddf1988608b5cec287d62372aee
alk3pInjection <webmaster@raspii.tech>
2020-09-05 15:37:14 +0800

    sm8150-common: configs: Import perfconfigstore.xml
    * Imported from stock ROM, with UXE enabled.
    
    Thanks @ NeonXeon
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- common.mk
+++ common.mk
@@ -370,6 +370,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
+# Performance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perfconfigstore.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
